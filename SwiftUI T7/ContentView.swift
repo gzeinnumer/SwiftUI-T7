450327d741dfedd60dc0d9fcb3181ab9cd449be9
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2022-03-11 01:52:16 +0700

    Initial commit

---
+++ SwiftUI T7/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI T7
+//
+//  Created by M Fadli Zein on 11/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI T7/SwiftUI_T7App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_T7App.swift
+//  SwiftUI T7
+//
+//  Created by M Fadli Zein on 11/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_T7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
